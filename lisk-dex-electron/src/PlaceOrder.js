84d2215c69316974080fb01522f3562264fc0a5c
Jackson Roberts <jacksonroberts25@gmail.com>
2020-01-03 03:20:40 -0700

    market/limit ui state

--- lisk-dex-electron/src/PlaceOrder.js
+++ lisk-dex-electron/src/PlaceOrder.js
@@ -6,7 +6,8 @@ export default class PlaceOrder extends React.Component {
     super(props);
     this.state = {
       price: 0,
-      amount: 0
+      amount: 0,
+      marketMode: true,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -28,13 +29,23 @@ export default class PlaceOrder extends React.Component {
     console.log(this.state.price);
   }
 
+  switchMode = () => {
+    this.setState({ marketMode: !this.state.marketMode });
+  }
+
   render() {
     return (
       <div style={{ padding: "5px" }}>
         <div className="action-name">{this.props.side.toUpperCase()}</div>
+        <button className="market-limit-buttons" disabled={this.state.marketMode} onClick={this.switchMode}>Market</button>
+        <button className="market-limit-buttons" disabled={!this.state.marketMode} onClick={this.switchMode}>Limit</button>
         <form onSubmit={this.handleSubmit}>
-          Price: <br></br>
-          <input name="price" className="order-val-input" type="text" value={this.state.price} onChange={this.handleChange} />
+          {!this.state.marketMode &&
+            <>
+              Price: <br></br>
+              <input name="price" className="order-val-input" type="text" value={this.state.price} onChange={this.handleChange} />
+            </>
+          }
           Amount: <br></br>
           <input name="amount" className="order-val-input" type="text" value={this.state.amount} onChange={this.handleChange} />
           <input className="place-order-button" type="submit" value="Submit" />
