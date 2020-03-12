8d6ecaafce1c7d72522da30aefc870ca99d69a32
Jackson Roberts <jacksonroberts25@gmail.com>
2020-03-11 18:23:34 -0600

    load orderbook initially from api.

--- lisk-dex-electron/src/API.ts
+++ lisk-dex-electron/src/API.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 export const API_URL = 'http://54.174.172.179:7011';
 
 
@@ -10,13 +10,7 @@ export function getClient(api_base_url: string) {
   });
 }
 
-const instance = axios.create({
-  baseURL: API_URL,
-  timeout: 10000,
-  headers: { 'X-LiskDexUI-Version': '0.2' }
-});
-
-export async function getOrderbook() {
+export async function getOrderbook(instance: AxiosInstance) {
   const orders = await instance.get('/orders?sort=price:asc');
   console.log("HIT API");
   return orders;
--- lisk-dex-electron/src/App.js
+++ lisk-dex-electron/src/App.js
@@ -7,7 +7,7 @@ import PlaceOrder from "./PlaceOrder";
 import YourOrders from "./YourOrders";
 import SignInModal from "./SignInModal";
 import SignInState from "./SignInState";
-import { getOrderbook } from "./API";
+import { getOrderbook, getClient } from "./API";
 import MarketList from "./MarketList";
 import { userContext } from './context';
 import * as cryptography from "@liskhq/lisk-cryptography";
@@ -24,8 +24,11 @@ class App extends React.Component {
     // This state has too many members. This is because we want to share data from API calls with various different components without
     // having to re-fetch the data in each.
     this.state = {
+      configuration: {},
       orderBookData: { orders: [], bids: [], asks: [], maxSize: { bid: 0, ask: 0 } },
       currentMarket: ["lsh", "lsk"],
+      // new, activeMarket string for selecting the active market out of the configuration object.
+      activeMarket: 'LSH/LSK',
       enabledAssets: ["lsk", "lsh"],
       displaySigninModal: false,
       signedIn: false,
@@ -51,154 +54,157 @@ class App extends React.Component {
     this.passphraseSubmit = this.passphraseSubmit.bind(this);
 
 
-    processConfiguration(defaultConfiguration);
-}
 
-refreshOrderbook = () => {
-  console.log('refreshing orderbook');
-  getOrderbook().then(results => {
-    const bids = [];
-    const asks = [];
-    let maxSize = { bid: 0, ask: 0 };
-    let myOrders = [];
-    for (let result of results.data) {
-      if (
-        // filter for the turrent trading pair.
-        (result.targetChain === this.state.currentMarket[0] || result.sourceChain === this.state.currentMarket[0]) &&
-        (result.targetChain === this.state.currentMarket[1] || result.sourceChain === this.state.currentMarket[1])
-      ) {
-        if (result.side === "bid") {
-          bids.push(result);
-          if (result.value > maxSize.bid) {
-            maxSize.bid = result.valueRemaining;
-          }
-          if (result.senderId === this.state.keys[this.state.currentMarket[1]]?.address) {
-            myOrders.push(result);
-          }
-        } else if (result.side === "ask") {
-          asks.push(result);
-          if (result.size > maxSize.ask) {
-            maxSize.ask = result.sizeRemaining;
-          }
-          if (result.senderId === this.state.keys[this.state.currentMarket[0]]?.address) {
-            myOrders.push(result);
+  }
+
+  refreshOrderbook = async () => {
+    this.setState({
+      configuration: (await processConfiguration(defaultConfiguration))
+    });
+    console.log('refreshing orderbook');
+    getOrderbook(getClient(this.state.configuration.markets[this.state.activeMarket].DEX_API_URL)).then(results => {
+      const bids = [];
+      const asks = [];
+      let maxSize = { bid: 0, ask: 0 };
+      let myOrders = [];
+      for (let result of results.data) {
+        if (
+          // filter for the turrent trading pair.
+          (result.targetChain === this.state.currentMarket[0] || result.sourceChain === this.state.currentMarket[0]) &&
+          (result.targetChain === this.state.currentMarket[1] || result.sourceChain === this.state.currentMarket[1])
+        ) {
+          if (result.side === "bid") {
+            bids.push(result);
+            if (result.value > maxSize.bid) {
+              maxSize.bid = result.valueRemaining;
+            }
+            if (result.senderId === this.state.keys[this.state.currentMarket[1]]?.address) {
+              myOrders.push(result);
+            }
+          } else if (result.side === "ask") {
+            asks.push(result);
+            if (result.size > maxSize.ask) {
+              maxSize.ask = result.sizeRemaining;
+            }
+            if (result.senderId === this.state.keys[this.state.currentMarket[0]]?.address) {
+              myOrders.push(result);
+            }
           }
         }
       }
-    }
-    console.log('my orders');
-    console.log(myOrders);
-    let maxBid = 0;
-    let minAsk = 0;
-    if (bids.length > 0) {
-      maxBid = bids[bids.length - 1].price;
-    }
-    if (asks.length > 0) {
-      minAsk = asks[0].price;
-    }
-    this.setState({ orderBookData: { bids, asks, maxSize }, maxBid, minAsk, myOrders });
-  });
-}
+      console.log('my orders');
+      console.log(myOrders);
+      let maxBid = 0;
+      let minAsk = 0;
+      if (bids.length > 0) {
+        maxBid = bids[bids.length - 1].price;
+      }
+      if (asks.length > 0) {
+        minAsk = asks[0].price;
+      }
+      this.setState({ orderBookData: { bids, asks, maxSize }, maxBid, minAsk, myOrders });
+    });
+  }
 
-componentDidMount() {
-  // Enable navigation prompt
-  window.onbeforeunload = (e) => {
-    //this.setDisplayLeaveWarning(true);
-    //e.preventDefault();
-    //return true;
-  };
+  componentDidMount() {
+    // Enable navigation prompt
+    window.onbeforeunload = (e) => {
+      //this.setDisplayLeaveWarning(true);
+      //e.preventDefault();
+      //return true;
+    };
 
-  this.refreshOrderbook();
-  setInterval(this.refreshOrderbook, 10000);
-}
+    this.refreshOrderbook();
+    setInterval(this.refreshOrderbook, 10000);
+  }
 
-showSignIn() {
-  this.setState({ displaySigninModal: true, signInFailure: false, });
-}
+  showSignIn() {
+    this.setState({ displaySigninModal: true, signInFailure: false, });
+  }
 
-passphraseSubmit(payload) {
-  const keys = {};
-  let atLeastOneKey = false;
-  for (const asset in payload) {
-    console.log(payload);
-    if (payload[asset] !== undefined) {
-      atLeastOneKey = true;
-      const passphrase = payload[asset].trim();
-      if (!Mnemonic.validateMnemonic(passphrase, Mnemonic.wordlists.english)) {
-        this.setState({ signInFailure: true });
-        return;
-      } else {
-        const address = (cryptography.getAddressAndPublicKeyFromPassphrase(passphrase)).address;
-        keys[asset] = { address, passphrase };
+  passphraseSubmit(payload) {
+    const keys = {};
+    let atLeastOneKey = false;
+    for (const asset in payload) {
+      console.log(payload);
+      if (payload[asset] !== undefined) {
+        atLeastOneKey = true;
+        const passphrase = payload[asset].trim();
+        if (!Mnemonic.validateMnemonic(passphrase, Mnemonic.wordlists.english)) {
+          this.setState({ signInFailure: true });
+          return;
+        } else {
+          const address = (cryptography.getAddressAndPublicKeyFromPassphrase(passphrase)).address;
+          keys[asset] = { address, passphrase };
+        }
       }
     }
+    if (atLeastOneKey) {
+      this.setState({ keys, signedIn: true, displaySigninModal: false });
+      this.refreshOrderbook();
+    }
   }
-  if (atLeastOneKey) {
-    this.setState({ keys, signedIn: true, displaySigninModal: false });
-    this.refreshOrderbook();
+
+  closeSignInModal = () => {
+    this.setState({ displaySigninModal: false });
   }
-}
 
-closeSignInModal = () => {
-  this.setState({ displaySigninModal: false });
-}
+  signOut = () => {
+    this.setState({ signedIn: false, keys: {} });
+  }
 
-signOut = () => {
-  this.setState({ signedIn: false, keys: {} });
-}
-
-setDisplayLeaveWarning = (val) => {
-  this.setState({ displayLeaveWarning: val });
-}
+  setDisplayLeaveWarning = (val) => {
+    this.setState({ displayLeaveWarning: val });
+  }
 
 
-render() {
-  return (
-    <userContext.Provider value={{ ...this.state }}>
-      {this.state.displaySigninModal && <SignInModal failure={this.state.signInFailure} passphraseSubmit={this.passphraseSubmit} enabledAssets={this.state.enabledAssets} close={this.closeSignInModal}></SignInModal>}
-      {this.state.displayLeaveWarning && <LeaveWarning setDisplayLeaveWarning={this.setDisplayLeaveWarning}></LeaveWarning>}
-      <div className="top-bar">
-        <div>
-          <b style={{ fontSize: '21px' }}>Lisk DEX</b> &nbsp;
+  render() {
+    return (
+      <userContext.Provider value={{ ...this.state }}>
+        {this.state.displaySigninModal && <SignInModal failure={this.state.signInFailure} passphraseSubmit={this.passphraseSubmit} enabledAssets={this.state.enabledAssets} close={this.closeSignInModal}></SignInModal>}
+        {this.state.displayLeaveWarning && <LeaveWarning setDisplayLeaveWarning={this.setDisplayLeaveWarning}></LeaveWarning>}
+        <div className="top-bar">
+          <div>
+            <b style={{ fontSize: '21px' }}>Lisk DEX</b> &nbsp;
             <a style={{ color: '#34cfeb', fontSize: '14px' }} href="https://github.com/Jaxkr/lisk-dex-ui/issues/new" rel="noopener noreferrer" target="_blank">Report bug / send feedback</a>
-          {/* eslint-disable-next-line jsx-a11y/accessible-emoji*/}
-          &nbsp; <span style={{ fontSize: '14px' }}>Thanks! 😊</span>
-        </div>
-        <div>
-          <SignInState showSignIn={this.showSignIn} keys={this.state.keys} signedIn={this.state.signedIn} signOut={this.signOut}></SignInState>
-        </div>
-      </div>
-      <div className="container">
-        <div className="buy-panel">
-          <PlaceOrder side="buy"></PlaceOrder>
-        </div>
-        <div className="sell-panel">
-          <PlaceOrder side="sell"></PlaceOrder>
-        </div>
-        <div className="orderbook-container">
-          <div className="sell-orders">
-            <Orderbook orderBookData={this.state.orderBookData} side="asks"></Orderbook>
+            {/* eslint-disable-next-line jsx-a11y/accessible-emoji*/}
+            &nbsp; <span style={{ fontSize: '14px' }}>Thanks! 😊</span>
           </div>
-          <div className="price-display">
-            Price: {this.state.maxBid} {this.state.currentMarket[1].toUpperCase()}
-          </div>
-          <div className="buy-orders">
-            <Orderbook orderBookData={this.state.orderBookData} side="bids"></Orderbook>
+          <div>
+            <SignInState showSignIn={this.showSignIn} keys={this.state.keys} signedIn={this.state.signedIn} signOut={this.signOut}></SignInState>
           </div>
         </div>
-        <div className="depth-chart">
-          <Chart whole={Math.pow(10, 8)} currentMarket={this.state.currentMarket}></Chart>
+        <div className="container">
+          <div className="buy-panel">
+            <PlaceOrder side="buy"></PlaceOrder>
+          </div>
+          <div className="sell-panel">
+            <PlaceOrder side="sell"></PlaceOrder>
+          </div>
+          <div className="orderbook-container">
+            <div className="sell-orders">
+              <Orderbook orderBookData={this.state.orderBookData} side="asks"></Orderbook>
+            </div>
+            <div className="price-display">
+              Price: {this.state.maxBid} {this.state.currentMarket[1].toUpperCase()}
+            </div>
+            <div className="buy-orders">
+              <Orderbook orderBookData={this.state.orderBookData} side="bids"></Orderbook>
+            </div>
+          </div>
+          <div className="depth-chart">
+            <Chart whole={Math.pow(10, 8)} currentMarket={this.state.currentMarket}></Chart>
+          </div>
+          <div className="your-orders">
+            <YourOrders orders={this.state.myOrders}></YourOrders>
+          </div>
+          <div className="market-name-and-stats">
+            <MarketList></MarketList>
+          </div>
         </div>
-        <div className="your-orders">
-          <YourOrders orders={this.state.myOrders}></YourOrders>
-        </div>
-        <div className="market-name-and-stats">
-          <MarketList></MarketList>
-        </div>
-      </div>
-    </userContext.Provider>
-  );
-}
+      </userContext.Provider>
+    );
+  }
 }
 
 export default App;
