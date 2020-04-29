b1a6b6537ccf7a84f477fce5272a1728d484b764
sacOO7 <sachinshinde7676@gmail.com>
2020-04-30 01:13:24 +0530

    [created test-data orderbook raw fixtures for calculating estimated value of returns corresponding to given asks or bids][references issue #3]

---
+++ lisk-dex-electron/src/__tests__/fixtures/orderbook4.js
@@ -0,0 +1,13 @@
+const asks = [
+  { price: 34, amount: 89 },
+  { price: 34, amount: 89 },
+  { price: 89, amount: 76 },
+];
+
+const bids = [
+  { price: 34, amount: 89 },
+  { price: 34, amount: 89 },
+  { price: 89, amount: 76 },
+];
+
+export { asks, bids };
