521c506c2636600ded41bc0c2d17717e5de8cb25
sacOO7 <sachinshinde7676@gmail.com>
2020-04-30 02:08:12 +0530

    [Created first orderbook based on realtime queue data][references issue #3]

--- lisk-dex-electron/src/__tests__/fixtures/orderbook1.js
+++ lisk-dex-electron/src/__tests__/fixtures/orderbook1.js
@@ -1,13 +1,20 @@
+// Price in LSK per leasehold, Amount in leaseholds
 const asks = [
-  { price: 34, amount: 89 },
-  { price: 34, amount: 89 },
-  { price: 89, amount: 76 },
+  { price: 0.9500, amount: 2000.0000 },
+  { price: 0.9000, amount: 913.4444 },
+  { price: 0.8700, amount: 66.7126 },
+  { price: 0.8500, amount: 6.4706 },
+  { price: 0.8000, amount: 10.0000 },
+  { price: 0.7700, amount: 3.1169 },
 ];
 
+// Price in LSH per lisk, Amount in lisks
 const bids = [
-  { price: 34, amount: 89 },
-  { price: 34, amount: 89 },
-  { price: 89, amount: 76 },
+  { price: 0.6000, amount: 91.7376 },
+  { price: 0.4000, amount: 62.6993 },
+  { price: 0.3000, amount: 8.7704 },
+  { price: 0.2500, amount: 15.0000 },
+  { price: 0.2000, amount: 10.0000 },
 ];
 
 export { asks, bids };
