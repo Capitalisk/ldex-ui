b472e6ab3ade8fd20fe3ebb830407e8a07f2dc15
sacOO7 <sachinshinde7676@gmail.com>
2020-05-16 23:45:11 +0530

    [updated eslintrc, added eslintignore to ignore unncessary folders] [refactored and applied lint fix on necessary files] [removed typescript and unnecessary packages from dev dependencies] [#23]

--- lisk-dex-electron/public/electron.js
+++ lisk-dex-electron/public/electron.js
@@ -1,10 +1,12 @@
-const electron = require('electron')
-const app = electron.app
-const path = require('path')
-const isDev = require('electron-is-dev')
-const BrowserWindow = electron.BrowserWindow
+// eslint-disable-next-line import/no-extraneous-dependencies
+const electron = require('electron');
 
-let mainWindow
+const { app } = electron;
+const path = require('path');
+
+const { BrowserWindow } = electron;
+
+let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -13,33 +15,32 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
     },
-  })
+  });
 
   mainWindow.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`,
-  )
-  mainWindow.webContents.on("new-window", function (event, url) {
+    `file://${path.join(__dirname, '../build/index.html')}`,
+  );
+
+  mainWindow.webContents.on('new-window', (event, url) => {
     event.preventDefault();
     electron.shell.openExternal(url);
   });
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
