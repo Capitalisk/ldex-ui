eafa42f31d21e418f00fc5e4331afeb2abd3a07f
sacOO7 <sachinshinde7676@gmail.com>
2020-05-26 01:16:14 +0530

    [updated circleci config. and artifacts isolator][#23]

--- lisk-dex-electron/ci-scripts/artifacts-isolator.sh
+++ lisk-dex-electron/ci-scripts/artifacts-isolator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd dist && mkdir -p compressed || exit && \
-zipped_artifatcts=$(find . -name "*.zip")
+zipped_artifatcts=$(find . -type f -maxdepth 1)
 for artifact in $zipped_artifatcts
 do
-  mv "$artifact" "$PWD/compressed/"
+  mv "$artifact" "$PWD/artifacts/"
 done
