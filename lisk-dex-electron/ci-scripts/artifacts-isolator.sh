3aa5f6b1caba930c248d76bbf4c939172f4e5672
sacOO7 <sachinshinde7676@gmail.com>
2020-05-26 13:51:48 +0530

    [updated artifcts isolator, updated config file][#23]

--- lisk-dex-electron/ci-scripts/artifacts-isolator.sh
+++ lisk-dex-electron/ci-scripts/artifacts-isolator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# Script to move all zipped/distribution files to artifacts
 cd dist && mkdir -p artifacts || exit && \
-zipped_artifatcts=$(find . -type f -maxdepth 1)
-for artifact in $zipped_artifatcts
-do
-  mv "$artifact" "$PWD/artifacts/"
+# shellcheck disable=SC1073
+find . -type f -maxdepth 1 -print0 | while IFS= read -r -d '' artifact; do
+    mv "$artifact" "$PWD/artifacts/"
 done
