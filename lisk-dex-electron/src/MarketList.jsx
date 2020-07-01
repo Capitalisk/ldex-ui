8784299664deab227cf3e99ac6972f2b49d4fcf1
sacOO7 <sachinshinde7676@gmail.com>
2020-07-02 00:26:42 +0530

    updated modal to properly show the table, updated market.js updated modal height, made it dynamic based on data

--- lisk-dex-electron/src/MarketList.jsx
+++ lisk-dex-electron/src/MarketList.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import './PlaceOrder.css';
-import InfoIcon from "./InfoIcon";
-import Modal from './Modal'
-import marketInfoDescriptor from './Market'
-import './Table.css'
+import InfoIcon from './InfoIcon';
+import Modal from './Modal';
+import marketInfoDescriptor from './Market';
+import './Table.css';
 
 export default class MarketList extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      modalOpened: false
-    }
+      modalOpened: false,
+    };
   }
 
   onInfoIconClick = () => {
-    this.setState({ modalOpened: true })
+    this.setState({ modalOpened: true });
   }
 
   modalClose = () => {
-    this.setState({ modalOpened: false })
+    this.setState({ modalOpened: false });
   }
 
   handleChange = (event) => {
@@ -40,55 +40,100 @@ export default class MarketList extends React.PureComponent {
     if (!confirmed) {
       e.preventDefault();
     } else {
-      this.props.signOut()
+      this.props.signOut();
     }
   }
 
   getModalContentFromConfig(config) {
     if (config) {
       const dexConfig = config.markets[this.props.activeMarket].dexOptions;
-      const ignoreKeysFromConfig = ["orderBookHash, processedHeights", "chainsWhitelist", "multisigMembers"];
-      const chains = dexConfig.chainsWhitelist;
+      // const ignoreKeysFromConfig = ['orderBookHash, processedHeights', 'chainsWhitelist'];
+      const chains = Object.keys(dexConfig.chains);
       const firstChain = dexConfig.chains[chains[0]];
       const secondChain = dexConfig.chains[chains[1]];
 
-      return <table style={{width: '-webkit-fill-available'}}>
-        <tr style={{'textTransform': 'uppercase'}}>
-          <th></th>
-          {chains.map((chain) => <th>{chain}</th>)}
-        </tr>
+      Object.entries(dexConfig.processedHeights).forEach(
+        ([chainName, value]) => { dexConfig.chains[chainName] = value; },
+      );
+
+      const getBasicTableRow = (firstCellValue, secondCellValue) => (
         <tr>
-          <td style={{fontWeight: 'bold'}}>Version</td>
-          <td colSpan={2} style={{textAlign: 'center', 'textTransform': 'uppercase'}}>{dexConfig.version}</td>
+          <td style={{ fontWeight: 'bold', border: 'none', width: '50%' }}>{firstCellValue}</td>
+          <td
+            colSpan={2}
+            style={{
+              textTransform: 'uppercase', border: 'none', width: '50%',
+            }}
+          >
+            {secondCellValue}
+          </td>
         </tr>
-        <tr>
-          <td style={{fontWeight: 'bold'}}>Base Chain</td>
-          <td colSpan={2} style={{textAlign: 'center', 'textTransform': 'uppercase'}}>{dexConfig.baseChain}</td>
-        </tr>
-        <tr>
-          <td style={{fontWeight: 'bold'}} >Price Decimal Precision</td>
-          <td colSpan={2} style={{textAlign: 'center', 'textTransform': 'uppercase'}}>{dexConfig.priceDecimalPrecision}</td>
-        </tr>
-        {
-          Object.keys(firstChain).map((chainInfoKey) => {
-            if (!ignoreKeysFromConfig.includes(chainInfoKey)) {
-              return <tr>
-                <td style={{fontWeight: 'bold'}}>{marketInfoDescriptor[chainInfoKey].name}</td>
-                <td>{firstChain[chainInfoKey]}</td>
-                <td>{secondChain[chainInfoKey]}</td>
+      );
+
+      return (
+        <div>
+          <table style={{ border: 'none', width: '60%' }}>
+            <tbody>
+              {getBasicTableRow('Version', dexConfig.version)}
+              {getBasicTableRow('Base-chain', dexConfig.baseChain)}
+              {getBasicTableRow('Price decimal precision', dexConfig.priceDecimalPrecision)}
+            </tbody>
+          </table>
+          <table style={{ width: '-webkit-fill-available', marginTop: '20px' }}>
+            <tbody>
+              <tr style={{ textTransform: 'uppercase' }}>
+                <th> </th>
+                {chains.map((chain) => <th>{chain}</th>)}
               </tr>
+              {
+              Object.keys(firstChain).map((chainInfoKey) => {
+                if (Array.isArray(firstChain[chainInfoKey])) {
+                  const firstChainValues = firstChain[chainInfoKey].join('\r\n');
+                  const secondChainValues = secondChain[chainInfoKey].join('\r\n');
+                  return (
+                    <tr>
+                      <td style={{ fontWeight: 'bold' }}>{marketInfoDescriptor[chainInfoKey].name}</td>
+                      <td style={{ whiteSpace: 'pre' }}>{firstChainValues}</td>
+                      <td style={{ whiteSpace: 'pre' }}>{secondChainValues}</td>
+                    </tr>
+                  );
+                }
+                let firstChainValue = firstChain[chainInfoKey];
+                let secondChainValue = secondChain[chainInfoKey];
+                const keyDescriptor = marketInfoDescriptor[chainInfoKey];
+                if ('mult' in keyDescriptor) {
+                  firstChainValue *= keyDescriptor.mult;
+                  firstChainValue += ' %';
+                  secondChainValue *= keyDescriptor.mult;
+                  secondChainValue += ' %';
+                }
+                if ('div' in keyDescriptor) {
+                  firstChainValue /= keyDescriptor.div;
+                  secondChainValue /= keyDescriptor.div;
+                }
+                return (
+                  <tr>
+                    <td style={{ fontWeight: 'bold' }}>{keyDescriptor.name}</td>
+                    <td>{firstChainValue}</td>
+                    <td>{secondChainValue}</td>
+                  </tr>
+                );
+              })
             }
-          })
-        }
-      </table>
+            </tbody>
+          </table>
+        </div>
+      );
     }
-    return <div/>
+    return <div />;
   }
 
   render() {
     return (
       <>
-x        <div style={{ padding: '10px' }}>
+        x
+        {' '}
+        <div style={{ padding: '10px' }}>
           <Modal modalOpened={this.state.modalOpened} closeModal={this.modalClose}>
             {this.getModalContentFromConfig(this.props.configuration)}
           </Modal>
@@ -99,19 +144,18 @@ x        <div style={{ padding: '10px' }}>
                 <p>
                   <b>
                     {
-                      marketSymbol === this.props.activeMarket ?
-                          marketSymbol.toUpperCase()
-                          :
-                          <a onClick={this.onMarketChange} href={`#market=${marketSymbol}`}>{marketSymbol.toUpperCase()}</a>
+                      marketSymbol === this.props.activeMarket
+                        ? marketSymbol.toUpperCase()
+                        : <a onClick={this.onMarketChange} href={`#market=${marketSymbol}`}>{marketSymbol.toUpperCase()}</a>
                     }
                   </b>
                   &nbsp;&nbsp;
-                  {marketSymbol === this.props.activeMarket && <InfoIcon alt="info" width="18px" marginBottom='-2px' onClick={this.onInfoIconClick} cursor='pointer'/>}
+                  {marketSymbol === this.props.activeMarket && <InfoIcon alt="info" width="18px" marginBottom="-2px" onClick={this.onInfoIconClick} cursor="pointer" />}
                 </p>
               </div>
             ))}
           </div>
-          <p style={{color: '#999999'}}>More markets coming soon!</p>
+          <p style={{ color: '#999999' }}>More markets coming soon!</p>
           <small>
             <br />
             Data refreshed every
