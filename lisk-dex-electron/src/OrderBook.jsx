ce11148763d6ebbc7348bb6bebd453c0eb4fb199
sacOO7 <sachinshinde7676@gmail.com>
2020-07-15 23:13:20 +0530

    Added title for asks and bids in orderbook

--- lisk-dex-electron/src/OrderBook.jsx
+++ lisk-dex-electron/src/OrderBook.jsx
@@ -4,53 +4,63 @@ import './OrderBook.css';
 import { Values } from './Utils';
 
 export default class OrderBook extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  render() {
-    const assets = this.props.assets || [];
-    const quoteSymbol = assets[0];
-    const baseSymbol = assets[1];
-    if (this.props.side === 'asks') {
-      const groupedAskOrders = Values(this.props.orderBookData.asks, 'price', 'sizeRemaining');
-      const orders = groupedAskOrders.map((ask) => (
-        <OrderBookEntry
-          whole={10 ** 8} // 10 ** 8 beddow in one LSK
-          key={ask.price}
-          size={ask.sizeRemaining}
-          price={ask.price}
-          maxSize={this.props.orderBookData.maxSize}
-          side={this.props.side}
-          sizeDecimals={4}
-          symbol={quoteSymbol}
-        />
-      ));
-      if (orders && orders.length > 0) {
-        return <div className="askOrderList">{orders}</div>;
-      }
-      return <div className="askOrderList"><div className="emptyAsk">No asks available</div></div>;
+    render() {
+        const assets = this.props.assets || [];
+        const quoteSymbol = assets[0];
+        const baseSymbol = assets[1];
+        if (this.props.side === 'asks') {
+            const groupedAskOrders = Values(this.props.orderBookData.asks, 'price', 'sizeRemaining');
+            const orders = groupedAskOrders.map((ask) => (
+                <OrderBookEntry
+                    whole={10 ** 8} // 10 ** 8 beddow in one LSK
+                    key={ask.price}
+                    size={ask.sizeRemaining}
+                    price={ask.price}
+                    maxSize={this.props.orderBookData.maxSize}
+                    side={this.props.side}
+                    sizeDecimals={4}
+                    symbol={quoteSymbol}
+                />
+            ));
+            if (orders && orders.length > 0) {
+                return (<div style={{height: "-webkit-fill-available", paddingTop: "10px"}}>
+                    <div style={{height: "10%", textAlign: "center"}}>Asks</div>
+                    <div className="askOrderList wrapAskOrderList">{orders}</div>
+                </div>);
+            }
+            return <div className="askOrderList">
+                <div className="emptyAsk">No asks available</div>
+            </div>;
+        }
+        if (this.props.side === 'bids') {
+            const groupedBidsOrders = Values(this.props.orderBookData.bids, 'price', 'valueRemaining');
+            const orders = groupedBidsOrders.map((bid) => (
+                <OrderBookEntry
+                    whole={10 ** 8} // 10 ** 8 beddow in one LSK
+                    key={bid.price}
+                    size={bid.valueRemaining}
+                    price={bid.price}
+                    maxSize={this.props.orderBookData.maxSize}
+                    side={this.props.side}
+                    sizeDecimals={4}
+                    symbol={baseSymbol}
+                />
+            ));
+            if (orders && orders.length > 0) {
+                return <div style={{height: "-webkit-fill-available", paddingBottom: "10px"}}>
+                    <div className="bidOrderList wrapBidOrderList">{orders}</div>
+                    <div style={{height: "4%", textAlign: "center"}}>Bids</div>
+                </div>;
+            }
+            return <div className="bidOrderList">
+                <div className="emptyBid">No bids available</div>
+            </div>;
+        }
+        return <></>;
     }
-    if (this.props.side === 'bids') {
-      const groupedBidsOrders = Values(this.props.orderBookData.bids, 'price', 'valueRemaining');
-      const orders = groupedBidsOrders.map((bid) => (
-        <OrderBookEntry
-          whole={10 ** 8} // 10 ** 8 beddow in one LSK
-          key={bid.price}
-          size={bid.valueRemaining}
-          price={bid.price}
-          maxSize={this.props.orderBookData.maxSize}
-          side={this.props.side}
-          sizeDecimals={4}
-          symbol={baseSymbol}
-        />
-      ));
-      if (orders && orders.length > 0) {
-        return <div className="bidOrderList">{orders}</div>;
-      }
-      return <div className="bidOrderList"><div className="emptyBid">No bids available</div></div>;
-    }
-    return <></>;
-  }
 }
