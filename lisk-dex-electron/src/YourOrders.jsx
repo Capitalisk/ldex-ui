cf70d06e3682922cafe9ee69bd6892e1cabec4a1
sacOO7 <sachinshinde7676@gmail.com>
2020-07-16 00:17:27 +0530

    Updated YourOrders to represent proper message for empty orders

--- lisk-dex-electron/src/YourOrders.jsx
+++ lisk-dex-electron/src/YourOrders.jsx
@@ -13,78 +13,101 @@ const statusValues = {
 export default class YourOrders extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  handleChange = (event) => {
-    const { target } = event;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const { name } = target;
+    handleChange = (event) => {
+      const { target } = event;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const { name } = target;
 
-    this.setState({
-      [name]: value,
-    });
-  }
+      this.setState({
+        [name]: value,
+      });
+    }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-  }
+    handleSubmit = (event) => {
+      event.preventDefault();
+    }
 
-  handleCancel = (order) => {
-    this.props.orderCanceled(order);
-  }
+    handleCancel = (order) => {
+      this.props.orderCanceled(order);
+    }
 
-  handleCancelFail = (error) => {
-    this.props.handleCancelFail(error);
-  }
+    handleCancelFail = (error) => {
+      this.props.handleCancelFail(error);
+    }
 
-  render() {
-    const orders = [...this.props.orders];
-    orders.sort((a, b) => {
-      const statusA = statusValues[a.status];
-      const statusB = statusValues[b.status];
-      if (statusA < statusB) {
-        return -1;
-      }
-      if (statusA > statusB) {
-        return 1;
-      }
-      const timeA = a.timestamp;
-      const timeB = b.timestamp;
-      if (timeA > timeB) {
-        return -1;
-      }
-      if (timeA < timeB) {
-        return 1;
-      }
-      return 0;
-    });
-    const bids = orders.filter((order) => order.side === 'bid').slice().reverse();
-    const asks = orders.filter((order) => order.side === 'ask');
-    return (
-      <>
-        <div style={{ padding: '5px' }}>
-          <div className="action-name">YOUR ORDERS</div>
-        </div>
-        <div style={{
-          width: '100%', margin: 0, padding: 0, display: 'flex',
-        }}
-        >
-          <div style={{
-            width: '50%', margin: 0, padding: 0, overflowY: 'scroll',
-          }}
-          >
-            {bids.slice().reverse().map((order) => <UserOrder key={order.id} side="bid" order={order} orderCanceled={this.handleCancel} failedToCancelOrder={this.handleCancelFail} />)}
+    render() {
+      const orders = [...this.props.orders];
+      orders.sort((a, b) => {
+        const statusA = statusValues[a.status];
+        const statusB = statusValues[b.status];
+        if (statusA < statusB) {
+          return -1;
+        }
+        if (statusA > statusB) {
+          return 1;
+        }
+        const timeA = a.timestamp;
+        const timeB = b.timestamp;
+        if (timeA > timeB) {
+          return -1;
+        }
+        if (timeA < timeB) {
+          return 1;
+        }
+        return 0;
+      });
+      const bids = orders.filter((order) => order.side === 'bid').slice().reverse();
+      const asks = orders.filter((order) => order.side === 'ask');
+      const askMessage = asks.length === 0 ? 'No sell orders placed' : 'Sell';
+      const bidsMessage = asks.length === 0 ? 'No buy orders placed' : 'Buy';
+      return (
+        <>
+          <div style={{ padding: '5px' }}>
+            <div className="action-name">YOUR ORDERS</div>
           </div>
           <div style={{
-            width: '50%', margin: 0, padding: 0, overflowY: 'scroll',
+            width: '100%', margin: 0, padding: 0, display: 'flex',
           }}
           >
-            {asks.map((order) => <UserOrder key={order.id} side="ask" order={order} orderCanceled={this.handleCancel} failedToCancelOrder={this.handleCancelFail} />)}
+            <div style={{
+              width: '50%', margin: 0, padding: 0,
+            }}
+            >
+              <div style={{ height: '10%', textAlign: 'center', marginTop: '10px' }}>{askMessage}</div>
+              <div style={{ overflowY: 'scroll', height: '90%' }}>
+                {bids.slice().reverse().map((order) => (
+                  <UserOrder
+                    key={order.id}
+                    side="bid"
+                    order={order}
+                    orderCanceled={this.handleCancel}
+                    failedToCancelOrder={this.handleCancelFail}
+                  />
+                ))}
+              </div>
+            </div>
+            <div style={{
+              width: '50%', margin: 0, padding: 0
+            }}
+            >
+              <div style={{ height: '10%', textAlign: 'center', marginTop: '10px' }}>{bidsMessage}</div>
+              <div style={{ overflowY: 'scroll', height: '90%' }}>
+                {asks.map((order) => (
+                  <UserOrder
+                    key={order.id}
+                    side="ask"
+                    order={order}
+                    orderCanceled={this.handleCancel}
+                    failedToCancelOrder={this.handleCancelFail}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      </>
-    );
-  }
+        </>
+      );
+    }
 }
